27611a5db73c82c3c5a3b4b50b4d735f07b25f03
Abdul Rahuman <abdul.rahuman@athleticgreens.com>
2024-12-16 23:23:07 -0500

    added rabbitmq for messaging

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -17,6 +17,9 @@ import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard/jwt.guard';
 import { Cache } from 'cache-manager';
 import { combineLatest } from 'rxjs';
+import { Client } from '@nestjs/microservices/external/nats-client.interface';
+import { ClientProxy } from '@nestjs/microservices';
+import e from 'express';
 
 @UseInterceptors(CacheInterceptor)
 @UseGuards(JwtGuard)
@@ -25,12 +28,17 @@ export class UserController {
   constructor(
     private authService: AuthService,
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    @Inject('NOTIFICATION_SERVICE') private notificationService: ClientProxy,
   ) {}
   @Get('user')
   async getUser(@GetUser() user: User) {
     await this.cacheManager.set('userid', user.id);
     await this.cacheManager.set('hi', 'stored in redis', 60);
-
+    const payload = {
+      userId: user.id,
+      email: user.email,
+    };
+    await this.notificationService.emit('notification', payload);
     console.log('Inside controller');
     console.log(await this.cacheManager.get('hi'));
     return await this.authService.getUser(user.id);
